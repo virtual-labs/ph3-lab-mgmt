59daea0f0773faa49c601805f0f7bfab8c28269b
ojas1 <mohrilop@gmail.com>
2020-03-21 10:53:22 +0530

    feat: footer update for phase 2

---
+++ footer.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const glob = require('glob');
+const path = require('path');
+const { JSDOM } = require("jsdom");
+
+const labpath = '../basic-engineering-mechanics-and-strength-of-materials-iiith/src/lab';
+const pattern = path.join(labpath, '*.html');
+console.log(pattern);
+
+const newFooterFile = './page-components/footer.html';
+
+function hasFooter(fn) {
+  const htmlContent = fs.readFileSync(fn);
+  let dom = new JSDOM(htmlContent);
+  if (dom.window.document.querySelector('footer')){
+    //console.log(fn);
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+function replaceFooter(pageFn, footerFn) {
+  const page = fs.readFileSync(pageFn, 'utf-8');
+  const footerContent = fs.readFileSync(footerFn, 'utf-8');
+  let dom = new JSDOM(page);
+  const footer = dom.window.document.querySelector('footer');
+  footer.id = 'footer';
+  footer.innerHTML = footerContent;
+  return dom.serialize();
+}
+
+glob(pattern, (err, fns) => {
+  const htmlPages = fns.filter(hasFooter);
+  const resPages = htmlPages.map((p) => [p, replaceFooter(p, newFooterFile)]);
+  resPages.forEach((p) => fs.writeFileSync(p[0], p[1], 'utf-8'));
+});
