6fccb382984cc24bb4e173c1aa8d88071993f475
ojas1 <mohrilop@gmail.com>
2021-01-07 22:53:39 +0530

    new compare

--- renderpage.js
+++ renderpage.js
@@ -49,8 +49,8 @@ function buildPage( data, current_item, template_content ) {
     data["menu"] = menu.filter((mi) => hasSource(mi));
 
     Handlebars.registerHelper('isActive', function (current_page, page) {
-	console.log(current_item, page);
-	return (current_page === page);
+	//console.log(current_item, page);
+	return (current_item.target === page);
     });
     
     const template = Handlebars.compile(template_content);
