d6368768193510fa7d39dcbe2c15272aacf2d33b
ojas1 <mohrilop@gmail.com>
2021-03-04 08:28:40 +0530

    fix: simulation iframe issues.

--- templates/assets/js/iframeResize.js
+++ templates/assets/js/iframeResize.js
@@ -1,9 +1,10 @@
-const sendPostMessage = () => {
-    
-    let height = document.body.offsetHeight;
-    window.parent.postMessage({
-	frameHeight: height
-    }, '*');
+const sendPostMessage = (mutationList, ob) => {    
+    if (mutationList) {
+	    let height = document.body.offsetHeight;
+	    window.parent.postMessage({
+		frameHeight: height
+	    }, '*');    	
+    }
 }
 
 window.onresize = () => sendPostMessage();
@@ -14,6 +15,10 @@ const observer = new MutationObserver(sendPostMessage);
 observer.observe(document.body, config);
 
 
+
+/* This is only needed when there is a nested iframe, and 
+will work only if this script is manualy inserted in the embedded iframe page. 
+*/
 window.onmessage = (e) => {
     if (e.data.hasOwnProperty("frameHeight")) {
 	var iframeDiv = document.querySelector("iframe");
