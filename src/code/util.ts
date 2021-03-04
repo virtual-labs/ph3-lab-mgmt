7fbaecce4158210c92b13692d514055b2127a65e
ojas1 <mohrilop@gmail.com>
2021-03-04 08:19:09 +0530

    removing deleted files

--- src/code/util.ts
+++
@@ -1,5 +0,0 @@
-export function firstToUpper(w: string) : string {
-    // converts first character of given word to uppercase.
-    // computer => Computer
-    return w[0].toUpperCase()+w.slice(1);
-}
--- src/code/validateLabDescriptor.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Ajv = require("ajv");
-var ajv = new Ajv();
-var validate = ajv.compile(require('./labDescSchema.json'));
-var valid = validate('../sample-lab-descriptor.json');
-if (!valid) {
-    console.log(ajv.errors);
-}
