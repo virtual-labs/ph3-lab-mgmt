19f8866c801660ee8b0917fbe756df6c71d9e49e
ojas1 <mohrilop@gmail.com>
2021-03-19 05:26:42 +0530

    config update

--- Config.js
+++ Config.js
@@ -1,6 +1,25 @@
 const path = require("path");
 const {BuildEnvs} = require("./Enums.js");
 
+const Lab = {
+  descriptor_name: "lab-descriptor",
+  build_dir: "build",
+  exp_dir: "experiments",
+  deployment_dest: "/var/www/html",
+  ui_template_name: "templates",
+  static_content_dir: "static_content",
+  partials: [
+    ["analytics_head", "lab-analytics-head"],
+    ["analytics_body", "lab-analytics-body"],
+    ["commons", "commons"],
+    ["header", "header"],
+    ["breadcrumbs", "breadcrumbs"],
+    ["side_menu", "sidemenu"],
+    ["content", "content"],
+    ["footer", "footer"]
+  ]
+}
+
 const Experiment = {
   descriptor_name: "experiment-descriptor",
   build_dir: "build",
@@ -25,9 +44,17 @@ const Experiment = {
   ]
 };
 
+
+/*
+Experiment build path
+-------------------------
+This is here to avoid circular dependency between
+Experiment moudle and Task module.
+*/
 function build_path(src) {
   return path.resolve(src, Experiment.build_dir, path.basename(path.resolve(src)));
 }
 
 module.exports.Experiment = Experiment;
+module.exports.Lab = Lab;
 module.exports.build_path = build_path;
