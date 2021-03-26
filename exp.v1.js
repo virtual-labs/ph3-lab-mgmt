aeef67ef1ac51e97c1c0f7e8653adfa9aaeee926
ojas1 <mohrilop@gmail.com>
2021-03-26 09:35:47 +0530

    exp process update

--- exp.v1.js
+++ exp.v1.js
@@ -1,9 +1,18 @@
 const path = require("path");
 const Handlebars = require("handlebars");
+const shell = require("shelljs");
+
 const {Experiment} = require("./Experiment.js");
+const Config = require("./Config.js");
 const {BuildEnvs, validBuildEnv} = require("./Enums.js");
 
 function run (src, lab_data, build_options) {
+
+  // if the experiment repo does not contain experiment descriptor we will add the default descriptor.
+  if (!shell.test("-f", Experiment.descriptorPath(src))){
+    shell.cp(path.resolve(Config.Experiment.default_descriptor), path.resolve(this.src, Experiment.descriptorPath(src)));
+  }
+
   const exp = new Experiment(src);
   exp.clean();
   exp.init(Handlebars);
@@ -12,6 +21,7 @@ function run (src, lab_data, build_options) {
 }
 
 module.exports.run = run;
+module.exports.build_experiment = run;
 
 if (require.main === module) {
   const args = require("minimist")(process.argv.slice(2));
@@ -33,7 +43,7 @@ if (require.main === module) {
     Anyways, for now, we will send an empty object as lab_data and
     hope things work out.
    */
-  const default_lab_data = {}; 
+  const default_lab_data = {};
   run(src, {}, build_options);
 }
 // node exp.v1.js --env=production ../
