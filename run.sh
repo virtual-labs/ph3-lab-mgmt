e39714bdc2003dade24fab98f6449787bc9434e5
Pavan <24794698+pavanchow@users.noreply.github.com>
2021-04-02 10:27:02 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,6 @@
 npm install
 npm run build-exp
 cd ../
-git config --local user.email "admin@vlabs.ac.in"
-git config --local user.name "vleadadmin"
-git checkout --orphan gh-pages
 git reset
 git add build/* -f
 git mv build/* ./ -f
