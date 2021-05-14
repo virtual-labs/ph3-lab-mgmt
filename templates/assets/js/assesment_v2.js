68e2c17f2767f4b1aa85206bfd00c3d2fe6415f1
vjspranav <pranavasri@live.in>
2021-05-14 21:40:28 +0530

    Correct answers based on filter * TODO: Add filter

--- templates/assets/js/assesment_v2.js
+++ templates/assets/js/assesment_v2.js
@@ -2,22 +2,41 @@ const quizContainer = document.getElementById("quiz");
 const resultsContainer = document.getElementById("results");
 const submitButton = document.getElementById("submit");
 
+const easyQuestions = [];
+const mediumQuestions = [];
+const hardQuestions = [];
+const difficulty_dict = {
+  easy: easyQuestions,
+  medium: mediumQuestions,
+  hard: hardQuestions,
+  all: myQuestions,
+};
+
+const populate_questions = () => {
+  myQuestions.forEach((currentQuestion) => {
+    if (currentQuestion.difficulty == "easy")
+      easyQuestions.push(currentQuestion);
+    if (currentQuestion.difficulty == "medium")
+      mediumQuestions.push(currentQuestion);
+    if (currentQuestion.difficulty == "hard")
+      hardQuestions.push(currentQuestion);
+  });
+};
 
 function showResults() {
   // gather answer containers from our quiz
   const answerContainers = quizContainer.querySelectorAll(".answers");
-  answerContainers.forEach(e => e.style.color = "black");
+  answerContainers.forEach((e) => (e.style.color = "black"));
 
+  let difficulty = "all";
   // keep track of user's answers
   let numCorrect = 0;
-
   // for each question...
-  myQuestions.forEach((currentQuestion, questionNumber) => {
+  difficulty_dict[difficulty].forEach((currentQuestion, questionNumber) => {
     // find selected answer
     const answerContainer = answerContainers[questionNumber];
     const selector = `input[name=question${questionNumber}]:checked`;
     const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
     // if answer is correct
     if (userAnswer === currentQuestion.correctAnswer) {
       // add to the number of correct answers
@@ -30,19 +49,20 @@ function showResults() {
       // color the answers red
       answerContainers[questionNumber].style.color = "red";
     }
-    let explanation = currentQuestion.explanations[userAnswer]
-    let explanation_div = document.getElementById("explanation"+ questionNumber.toString())
-    if(explanation){
-      explanation_div.innerHTML = "Explanation: " + explanation
-      explanation_div.style.display = "block"
-    }else{
-      explanation_div.style.display = "none"
+    let explanation = currentQuestion.explanations[userAnswer];
+    let explanation_div = document.getElementById(
+      "explanation" + questionNumber.toString()
+    );
+    if (explanation) {
+      explanation_div.innerHTML = "Explanation: " + explanation;
+      explanation_div.style.display = "block";
+    } else {
+      explanation_div.style.display = "none";
     }
   });
-
   // show number of correct answers out of total
-  resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+  resultsContainer.innerHTML = `${numCorrect} out of ${difficulty_dict[difficulty].length}`;
 }
 
-
+populate_questions();
 submitButton.addEventListener("click", showResults);
