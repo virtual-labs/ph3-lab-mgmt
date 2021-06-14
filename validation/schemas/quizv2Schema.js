1cdf3f18f95321d98bf61fe5b46e1c8d9f7e65cb
vjspranav <pranavasri@live.in>
2021-06-15 00:55:08 +0530

    Working implementations of v1 and v2 schema

--- validation/schemas/quizv2Schema.js
+++ validation/schemas/quizv2Schema.js
@@ -1,12 +1,57 @@
 module.exports = {
-  type: "object",
-  properties: {
-    version: {
-      type: "integer",
-      enum: [2],
-      errorMessage: "Version should be 2.0",
+  oneOf: [
+    {
+      type: "object",
+      properties: {
+        version: {
+          type: "integer",
+          enum: [2],
+          errorMessage: "Version should be 2.0",
+        },
+        questions: {
+          type: "array",
+          items: {
+            type: "object",
+            properties: {
+              question: {
+                type: "string",
+              },
+              answers: {
+                type: "object",
+                minProperties: 1,
+                errorMessage: {
+                  minProperties: "There have to be atleast one correct answer",
+                },
+              },
+              correctAnswer: {
+                type: "string",
+              },
+              difficulty: {
+                type: "string",
+                enum: ["beginner", "intermediate", "advanced"],
+                errorMessage: {
+                  enum:
+                    "Difficulty can only be: beginner, intermediate or advanced",
+                },
+              },
+              explanations: {
+                type: "object",
+              },
+            },
+            additionalProperties: false,
+            required: ["question", "answers", "correctAnswer", "difficulty"],
+          },
+          minItems: 1,
+          errorMessage: {
+            minItems: "Questions cannot be empty",
+            type: "Please provide an array of questions",
+          },
+        },
+      },
+      required: ["version", "questions"],
+      additionalProperties: false,
     },
-    questions: {
+    {
       type: "array",
       items: {
         type: "object",
@@ -16,36 +61,18 @@ module.exports = {
           },
           answers: {
             type: "object",
-            minItems: 1,
+            minProperties: 1,
             errorMessage: {
-              minItems: "There have to be atleast one correct answer",
+              minProperties: "There have to be atleast one correct answer",
             },
           },
           correctAnswer: {
             type: "string",
           },
-          difficulty: {
-            type: "string",
-            enum: ["beginner", "intermediate", "advanced"],
-            errorMessage: {
-              enum:
-                "Difficulty can only be: beginner, intermediate or advanced",
-            },
-          },
-          explanations: {
-            type: "object",
-          },
         },
-        additionalProperties: false,
-        required: ["question", "answers", "correctAnswer", "difficulty"],
-      },
-      minItems: 1,
-      errorMessage: {
-        minItems: "Questions cannot be empty",
-        type: "Please provide an array of questions",
+        required: ["question", "answers", "correctAnswer"],
       },
     },
-  },
-  required: ["version", "questions"],
-  additionalProperties: false,
+  ],
+  errorMessage: { anyOf: "The json does not validate with version 1 or 2" },
 };
