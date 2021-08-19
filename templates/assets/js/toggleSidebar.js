aea3fbfc8fc72a8b0577d81166018da0b9c5feda
Shlok Pandey <31105595+b30wulffz@users.noreply.github.com>
2021-08-19 23:26:32 +0530

    Add nav to simulation page, tweak lab landing page (#50)
    * Add nav to simulation page, tweak lab landing page
    
    * Move http to https
    
    * Move title to center
    
    * Rename class, fix url

--- templates/assets/js/toggleSidebar.js
+++ templates/assets/js/toggleSidebar.js
@@ -16,3 +16,21 @@ function toggle() {
     }
   }
 }
+
+function simulationHeaderToggle() {
+  const dropdown = document.querySelector(".simulation-header .dropdown .backdrop");
+  if(dropdown.classList.contains("vlabs-hidden") ) {
+    dropdown.classList.remove("vlabs-hidden");
+  }
+  else {
+    dropdown.classList.add("vlabs-hidden");
+  }
+}
+
+if(document.querySelector(".simulation-header")){
+  window.addEventListener('click',({ target }) => {
+    if (!(target.closest('.navbar-toggler') || target.closest('.nav-menu'))) {
+      document.querySelector(".simulation-header .dropdown .backdrop").classList.add("vlabs-hidden");
+    }
+  });
+}
