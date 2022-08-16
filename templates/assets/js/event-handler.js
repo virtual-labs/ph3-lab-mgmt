b92264e06bd13a2911f5fb7c9abd390186774fda
vjspranav <pranavasri@live.in>
2022-08-17 04:52:35 +0530

    Push learningUnit and task to dataLayer

--- templates/assets/js/event-handler.js
+++ templates/assets/js/event-handler.js
@@ -2,9 +2,13 @@
 
 document.getElementById('bug-report').addEventListener('vl-bug-report', (e)=>{
     if(e.detail.status === 200 || e.detail.status === 201){
+        const learningUnit = document.head.querySelector('meta[name="learning-unit"]').content;
+        const task = document.head.querySelector('meta[name="task-name"]').content;
         dataLayer.push({
             event: "vl-bug-report",
-            "bug-type": e.detail.issues  
+            "bug-type": e.detail.issues,
+            "learning-unit": learningUnit ? learningUnit : "", 
+            "task-name": task ? task : ""
         })
         alert("Bug Reported successfully");
     }else{
