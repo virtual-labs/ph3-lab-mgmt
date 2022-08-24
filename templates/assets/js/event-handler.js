cd463c46cc60791ef40455bb3d6ae1929359fa4c
vjspranav <pranavasri@live.in>
2022-08-25 02:16:26 +0530

    Add toast mesages instead of alerts * They are technically still alerts of bootstrap, but they look more like a toast * Also fix duplicate question

--- templates/assets/js/event-handler.js
+++ templates/assets/js/event-handler.js
@@ -10,8 +10,35 @@ document.getElementById('bug-report').addEventListener('vl-bug-report', (e)=>{
             "learning-unit": learningUnit ? learningUnit : "", 
             "task-name": task ? task : ""
         })
-        alert("Bug Reported successfully");
+        // alert("Bug Reported successfully");
+        // Create a toast notification use bootstrap
+        const toast = document.createElement('div');
+        toast.classList.add('alert', 'show', 'alert-success', 'alert-dismissible', 'fade');
+        toast.setAttribute('role', 'alert');
+        //set  style="position: absolute; top: 0; right: 0;
+        toast.setAttribute('style', 'position: absolute; top: 0; z-index: 9999; width: 100%'); 
+        toast.innerHTML = `Bug reported successfully
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>`;
+        document.body.appendChild(toast);
+        setTimeout(()=>{
+            toast.classList.remove('show');
+        } , 3000);
     }else{
-        alert(e.detail.message)
+        // Create a try again toast notification use bootstrap
+        const toast = document.createElement('div');
+        toast.classList.add('alert', 'show', 'alert-danger', 'alert-dismissible', 'fade');
+        toast.setAttribute('role', 'alert');
+        //set  style="position: absolute; top: 0; right: 0;
+        toast.setAttribute('style', 'position: absolute; top: 0; z-index: 9999; width: 100%'); 
+        toast.innerHTML = `Please Try Again
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>`;
+        document.body.appendChild(toast);
+        setTimeout(()=>{
+            toast.classList.remove('show');
+        } , 3000);
     }
 })
