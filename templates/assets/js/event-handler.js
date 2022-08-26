2fd786b16e3931c983569d6660083c0e89cd3823
vjspranav <pranavasri@live.in>
2022-08-26 13:50:45 +0530

    Use Sweet alert 2 for toast notification

--- templates/assets/js/event-handler.js
+++ templates/assets/js/event-handler.js
@@ -1,5 +1,17 @@
 "use-strict";
 
+const Toast = Swal.mixin({
+    toast: true,
+  position: 'bottom-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }
+})
+
 document.getElementById('bug-report').addEventListener('vl-bug-report', (e)=>{
     if(e.detail.status === 200 || e.detail.status === 201){
         const learningUnit = document.head.querySelector('meta[name="learning-unit"]').content;
@@ -9,36 +21,21 @@ document.getElementById('bug-report').addEventListener('vl-bug-report', (e)=>{
             "bug-type": e.detail.issues,
             "learning-unit": learningUnit ? learningUnit : "", 
             "task-name": task ? task : ""
+        })        
+        Toast.fire({
+          icon: 'success',
+          iconColor: "white",
+          background:"#a5dc86",
+          title: 'Bug Reported Succesfully',
         })
-        // alert("Bug Reported successfully");
-        // Create a toast notification use bootstrap
-        const toast = document.createElement('div');
-        toast.classList.add('alert', 'show', 'alert-success', 'alert-dismissible', 'fade');
-        toast.setAttribute('role', 'alert');
-        //set  style="position: absolute; top: 0; right: 0;
-        toast.setAttribute('style', 'position: absolute; top: 0; z-index: 9999; width: 100%'); 
-        toast.innerHTML = `Bug reported successfully
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>`;
-        document.body.appendChild(toast);
-        setTimeout(()=>{
-            toast.classList.remove('show');
-        } , 3000);
     }else{
-        // Create a try again toast notification use bootstrap
-        const toast = document.createElement('div');
-        toast.classList.add('alert', 'show', 'alert-danger', 'alert-dismissible', 'fade');
-        toast.setAttribute('role', 'alert');
-        //set  style="position: absolute; top: 0; right: 0;
-        toast.setAttribute('style', 'position: absolute; top: 0; z-index: 9999; width: 100%'); 
-        toast.innerHTML = `Please Try Again
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>`;
-        document.body.appendChild(toast);
-        setTimeout(()=>{
-            toast.classList.remove('show');
-        } , 3000);
+        Toast.fire({
+            icon: 'error',
+            iconColor: "white", 
+            color:"white",
+            background:"#f27474",
+            timer: 5000,
+            title: 'Bug Report Failed, Please Try Again',
+        })
     }
 })
