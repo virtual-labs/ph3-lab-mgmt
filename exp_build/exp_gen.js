4633125cbd86c7822a7ca8604ee1e32ad82eb76c
r <raj@vlabs.ac.in>
2024-04-05 10:03:32 +0530

    Fixed bugs related to icons and build errors

--- exp_build/exp_gen.js
+++ exp_build/exp_gen.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const path = require("path");
 const Handlebars = require("handlebars");
 const shell = require("shelljs");
@@ -8,12 +9,25 @@ const { Plugin } = require("./plugin.js");
 const { PluginScope } = require("../enums.js");
 const log = require("../logger.js");
 
+
+function copyFileSync(src, dest, options = {}) {
+
+  try {
+    fs.copyFileSync(src, dest);
+  } catch (e) {
+    log.error(`Error while copying ${src} to ${dest}::${e}`);
+    return 1;
+  }
+  return 0;
+}
+
 function run(src, lab_data, build_options) {
   log.debug(`Running build process at ${src}`);
   // if the experiment repo does not contain experiment descriptor we will add the default descriptor.
   if (!shell.test("-f", Experiment.descriptorPath(src))) {
-    shell.cp(
-      path.resolve(Config.Experiment.default_descriptor),
+    // shell.cp(
+    copyFileSync(
+      path.resolve(Config.PROJECT_ROOT, Config.Experiment.default_descriptor),
       path.resolve(this.src, Experiment.descriptorPath(src))
     );
   }
@@ -27,7 +41,7 @@ function run(src, lab_data, build_options) {
   );
 
   pageScopePlugins.forEach((plugin) => {
-    if(plugin.id === "svc-rating") {
+    if (plugin.id === "svc-rating") {
       plugin.attributes.columnValue = lab_data.exp_short_name;
     }
   });
@@ -35,22 +49,20 @@ function run(src, lab_data, build_options) {
   const code_assessment = exp.descriptor["code-assessment"];
 
   // Include code-assessment.json if the code editor is included
-  if(code_assessment?.include)
-  {
-    log.info("Code Editor included")
+  if (code_assessment?.include) {
+    log.info("Code Editor included");
     build_options.codeditor = true;
     build_options.code_assessment = code_assessment;
     exp.includeCodeEditor(code_assessment.position);
   }
-  else{
+  else {
     build_options.codeditor = false;
-    log.info("Code Editor Not included")
+    log.info("Code Editor Not included");
   }
-  
+
   exp.init(Handlebars);
   // Validation
-  if (build_options.isValidate)
-  {
+  if (build_options.isValidate) {
     exp.validate(build_options);
   }
 
